c25fc9b33bab43d15194d1b4e1ebca63f91bf5a3
Bryant Hayes <bryantfhayes@gmail.com>
2019-04-27 16:16:34 -0700

    Update hover text

--- Game/scripts/IA_Bloodbank.gd
+++ Game/scripts/IA_Bloodbank.gd
@@ -10,6 +10,7 @@ var offset = Vector2(10.0, -25.0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
+	get_node("InteractLbl").text = "Press 'e' to Enter Bloodbank"
 	get_node("InteractLbl").visible = false
 	pass # Replace with function body.
 
@@ -18,11 +19,13 @@ func _process(delta):
 	if attachedBody != null:
 		get_node("InteractLbl").rect_global_position = attachedBody.position + offset
 		if Input.is_action_just_pressed("ui_interact"):
-			print("INTERACT")
+			print("INTERACTED WITH BLOODBANK")
 
 func _on_Area2D_body_entered(body):
-	attachedBody = body
-	get_node("InteractLbl").visible = true
+	if body.get_name() == "Player":
+		print("ENTER")
+		attachedBody = body
+		get_node("InteractLbl").visible = true
 
 func _on_Area2D_body_exited(body):
 	attachedBody = null
