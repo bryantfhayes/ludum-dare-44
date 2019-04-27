e146dc3131f8576919356491bd660ea81175c242
Bryant Hayes <bryantfhayes@gmail.com>
2019-04-27 00:44:11 -0700

    Add tilemap and player than moves around

---
+++ Game/scripts/Player.gd
@@ -0,0 +1,55 @@
+extends Area2D
+
+signal hit
+
+# How fast the player will move (pixels/sec)
+export (int) var speed
+
+# Size of the game window
+var screensize
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	screensize = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.animation = "right"
+		velocity.x += 1
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.animation = "left"
+		velocity.x -= 1
+	elif Input.is_action_pressed("ui_down"):
+		$AnimatedSprite.animation = "down"
+		velocity.y += 1
+	elif Input.is_action_pressed("ui_up"):
+		$AnimatedSprite.animation = "up"
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.frame = 0
+		$AnimatedSprite.stop()
+		
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
+
+func _on_Player_body_entered(body):
+	print("Collided")
+
+
+
+func _on_Player_area_entered(area):
+	print("collide")
+	pass # replace with function body
