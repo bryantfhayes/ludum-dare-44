8a507732242e7a9af1e01d5e51ae1a14575ee284
MaxMSchweitzer <mschwei2@pdx.edu>
2019-04-27 20:51:03 -0700

    Add interest upgrade, fix bank label

--- Game/scripts/IA_Bank.gd
+++ Game/scripts/IA_Bank.gd
@@ -6,20 +6,27 @@ extends Area2D
 
 var attachedBody = null
 var offset = Vector2(10.0, -25.0)
+var interest_gain = 0.01
+var interest_upgrade = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
-	get_node("InteractLbl").text = "Press 'e' to Enter Bank"
+	var text = "Press 'e' to increase interest \n rate (%.3f for %d gold coins)"
+	get_node("InteractLbl").text = text % [interest_gain, interest_upgrade]
 	get_node("InteractLbl").visible = false
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if attachedBody != null:
-		
+		var node = self.owner
+		var player_node = node.get_node("Player")
+		var purse_node = player_node.get_node("Purse")
 		if Input.is_action_just_pressed("ui_interact"):
-			print("INTERACTED WITH BANK")
+			if purse_node.gold - interest_upgrade >= 0:
+				purse_node.remove_gold(interest_upgrade)
+				purse_node.interest_rate += interest_gain
 
 func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player":
