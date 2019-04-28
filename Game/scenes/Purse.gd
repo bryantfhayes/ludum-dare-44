c5dd6b1f7a9cbbae2e25571fb032899fd0db0205
Bryant Hayes <bryantfhayes@gmail.com>
2019-04-28 13:20:20 -0700

    add firebase support

--- Game/scenes/Purse.gd
+++ Game/scenes/Purse.gd
@@ -8,14 +8,17 @@ export (int) var starting_gold = 0
 
 var gold = 11
 var timeSinceLastInterest = 0
+var playtime = 0
 
 func _ready():
 	gold = starting_gold
 	set_process(true)
 	emit_signal("gold_changed", gold)
+	var retval = GameManager.connect("save_score", self, "_on_Purse_save_score")
 
 func _process(delta):
 	timeSinceLastInterest += delta
+	playtime += delta
 	if timeSinceLastInterest >= seconds_between_interest:
 		timeSinceLastInterest = 0
 		var interest = interest_rate * gold
@@ -28,3 +31,7 @@ func add_gold(amount):
 func remove_gold(amount):
 	gold -= amount
 	emit_signal("gold_changed", gold)
+	
+func _on_Purse_save_score(uuid):
+	var requester = self.owner.get_node("HTTPRequest")
+	requester.request("https://veins-of-gold.firebaseio.com/highscores.json", [], true, HTTPClient.METHOD_PATCH, "{\"%s\" : {\"score\" : %f, \"playtime\" : %f }}" % [uuid, gold, playtime])
--- Game/scripts/GameManager.gd
+++ Game/scripts/GameManager.gd
@@ -5,16 +5,52 @@ extends Node
 # var b = "text"
 
 signal regen_blood(amount)
+signal save_score(uuid)
 
 # Blood drain in mL every second
 var bloodRegenMlPerSecond = 100
 var timeSinceLastRegen = 0
 
+var timeSinceLastHeartbeat = 0
+var secondsBetweenHeartbeats = 2
+var uuid = ""
+
+static func getRandomInt(max_value):
+  randomize()
+
+  return randi() % max_value
+
+static func randomBytes(n):
+  var r = []
+
+  for index in range(0, n):
+    r.append(getRandomInt(256))
+
+  return r
+
+static func uuidbin():
+  var b = randomBytes(16)
+
+  b[6] = (b[6] & 0x0f) | 0x40
+  b[8] = (b[8] & 0x3f) | 0x80
+  return b
+
+static func v4():
+  var b = uuidbin()
+
+  var low = '%02x%02x%02x%02x' % [b[0], b[1], b[2], b[3]]
+  var mid = '%02x%02x' % [b[4], b[5]]
+  var hi = '%02x%02x' % [b[6], b[7]]
+  var clock = '%02x%02x' % [b[8], b[9]]
+  var node = '%02x%02x%02x%02x%02x%02x' % [b[10], b[11], b[12], b[13], b[14], b[15]]
+
+  return '%s-%s-%s-%s-%s' % [low, mid, hi, clock, node]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_music()
 	set_process(true)
-	pass # Replace with function body.
+	uuid = v4()
 
 func start_music():
 	var player = AudioStreamPlayer.new()
@@ -26,7 +62,11 @@ func start_music():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	timeSinceLastRegen = timeSinceLastRegen + delta
+	timeSinceLastHeartbeat += delta
 	if timeSinceLastRegen >= 1.0:
 		emit_signal("regen_blood", bloodRegenMlPerSecond)
 		timeSinceLastRegen = 0
-	pass
+	if timeSinceLastHeartbeat >= secondsBetweenHeartbeats:
+		timeSinceLastHeartbeat = 0
+		emit_signal("save_score", uuid)
+
