614425c6931f973e890dff6a56dfeb4cc7aaa4c6
MaxMSchweitzer <mschwei2@pdx.edu>
2019-04-27 20:07:50 -0700

    Testing game over

---
+++ Game/scenes/ColorRect.gd
@@ -0,0 +1,31 @@
+extends ColorRect
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var node
+var player_node
+var health_node
+var max_health
+var opacity_thresh_hold = 0.15
+var opacity_percent = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Health_health_changed(health):
+	node = self.owner
+	player_node = node.get_node("Player")
+	health_node = player_node.get_node("Health")
+	max_health = health_node.max_health
+	var opacity_threshold = max_health * opacity_thresh_hold
+	if health_node.health < opacity_threshold:
+		self.modulate.a = (opacity_threshold - health_node.health) * .100
+	
+	pass # Replace with function body.
