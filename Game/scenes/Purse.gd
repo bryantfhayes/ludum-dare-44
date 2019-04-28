b3b624bd989f92121e2fa024221dda288e0fab3b
Bryant Hayes <bryantfhayes@gmail.com>
2019-04-27 19:25:20 -0700

    Add bank interest

--- Game/scenes/Purse.gd
+++ Game/scenes/Purse.gd
@@ -2,10 +2,24 @@ extends Node
 
 signal gold_changed(gold)
 
+export (float) var seconds_between_interest = 1.0
+export (float) var interest_rate = 0.02
+export (int) var starting_gold = 0
+
 var gold = 11
+var timeSinceLastInterest = 0
 
 func _ready():
+	gold = starting_gold
+	set_process(true)
 	emit_signal("gold_changed", gold)
+
+func _process(delta):
+	timeSinceLastInterest += delta
+	if timeSinceLastInterest >= seconds_between_interest:
+		timeSinceLastInterest = 0
+		var interest = interest_rate * gold
+		add_gold(interest)
 
 func add_gold(amount):
 	gold += amount
